694b4085a47d7d5d816b11f88ff3bb86e90c5482
Daniel Adepoju <adebalanced02@gmail.com>
2022-05-22 20:11:41 +0100

    :house: Project cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./styles/styles.scss";
 import reportWebVitals from './reportWebVitals';
+import Home from './pages';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Home = (): React.ReactElement => <div className="home">Home Page</div>
+
+export default Home
